214a8e04c7da442088a518d1b521846a8e754824
Michiel Hildebrand <m.hildebrand@vu.nl>
2014-09-02 13:03:19 +0200

    Add missing namespace declaration

--- config-available/entity_proxy.pl
+++ config-available/entity_proxy.pl
@@ -3,3 +3,8 @@
 /** <module> Entity proxy to crawl and serve information about entities
 */
 
+:- use_module(library(semweb/rdf_db)).
+
+:- rdf_register_ns(linkedtv, 'http://data.linkedtv.eu/ontology/').
+
+:- use_module(api(entity_proxy)).
